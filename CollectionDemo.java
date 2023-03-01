d755969507863fda0bc58bfbd9b7a65211938e52
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-01 23:31:30 +0500

    Some basic operation on collection

--- CollectionDemo.java
+++ CollectionDemo.java
@@ -1,7 +1,21 @@
 package Collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class CollectionDemo {
     public static void show(){
+        Collection<String> collection = new ArrayList<>();
+        collection.add("a");
+        collection.add("b");
+        collection.add("c");
+        for (var item:collection)
+            System.out.println(item);
+        System.out.println(collection);
+        Collections.addAll(collection,"d","e","f");
+        System.out.println(collection);
+        System.out.println(collection.size());
 
     }
 }
