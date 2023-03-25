faf125040184cab56907587ea51e4226cc3d6739
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-25 18:09:53 +0500

    Tutorial on Sets

--- SetDemo.java
+++ SetDemo.java
@@ -1,12 +1,31 @@
 package Collection;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SetDemo {
     public static void show(){
+        Collection<String> collection = new ArrayList<>();
+        Collections.addAll(collection,"a","b","c","c");
         Set<String> set = new HashSet<>();
-        System.out.println("");
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        set.add("a");
+        System.out.println(set);
+        Set<String> sett = new HashSet<>(collection);
+        System.out.println(sett);
+//        Set Operation
+        Set<String> set1 = new HashSet<>(Arrays.asList("a","b","c"));
+        Set<String> set2 = new HashSet<>(Arrays.asList("c","d","e"));
+//        Union
+//        set1.addAll(set2);
+//        Intersection
+//        set1.retainAll(set2);
+//        Difference
+//        set1.removeAll(set2);
+        System.out.println(set1);
+
+
+
     }
 }
